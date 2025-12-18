2167057deb25c7b04b0d3346b503a2955db4509b
maruyama <taku2000maru@icloud.com>
2025-12-19 00:44:12 +0900

    定数

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CLI-Geographic-Calculation
+
+# 国土数値情報データは giodata 内部に格納すること
---
+++ internal/giocal/geographic_constant.go
@@ -0,0 +1,9 @@
+// 定数
+
+// 1kmあたりの経度 緯度（東京付近）
+package giocal
+
+const (
+	LongitudePerKm = 0.010966404715491394 // 1kmあたりの経度（東京付近）
+	LatitudePerKm  = 0.009013372974576 // 1kmあたりの緯度（東京付近）
+)
