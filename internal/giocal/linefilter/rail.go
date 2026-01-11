9b6b25343df50ea99ff652df64cc32d2d0896e33
maruyama <taku2000maru@icloud.com>
2026-01-12 04:54:03 +0900

    feature : linefilterの作成

---
+++ internal/giocal/linefilter/rail.go
@@ -0,0 +1,34 @@
+package linefilter
+
+import "CLI-Geographic-Calculation/internal/giocal/giocaltype"
+
+func FilterRailroadSectionByProperties(railroadSections *[]giocaltype.GiotypeRailroadSection, property string , value []string) []int{
+	// RailroadLinePropertyMap から対応するプロパティコードを取得
+
+	// propertyCodeがvalueに含まれるかどうかをチェックその該当する行番号を返す
+	matchingIndices := []int{}
+	for i, section := range *railroadSections {
+		
+		// 型安全に
+		// propValue, exists := section.Properties[propertyCode]できないのでswqitch文で対応
+		var propValue string
+		switch property {
+			// caseはSQL文で使うカラム名に対応させる
+		case "company":
+			propValue = section.Properties.N02003
+		case "line":
+			propValue = section.Properties.N02004
+		default:
+			continue // 未知のプロパティコードの場合はスキップ
+		}
+
+		// valueにpropValueが含まれるかチェック
+		for _, v := range value {
+			if propValue == v {
+				matchingIndices = append(matchingIndices, i)
+				break
+			}
+		}
+	}
+	return matchingIndices
+}
